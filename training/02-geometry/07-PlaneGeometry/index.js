06b3a5db0db8e4445b2412167f64bbf818b1e175
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-18 19:06:43 -0500

    add ring, plane, torus, trous knot geometry

---
+++ training/02-geometry/07-PlaneGeometry/index.js
@@ -0,0 +1,101 @@
+import * as THREE from "../../../build/three.module.js";
+import { OrbitControls } from "../../../examples/jsm/controls/OrbitControls.js";
+
+class App {
+	constructor() {
+		const divContainer = document.getElementById("webgl-container");
+		this._divContainer = divContainer;
+
+		const renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.setPixelRatio(window.devicePixelRatio);
+		divContainer.appendChild(renderer.domElement);
+		this._renderer = renderer;
+
+		const scene = new THREE.Scene();
+		this._scene = scene;
+
+		this._setupCamera();
+		this._setupLight();
+		this._setupModel();
+		this._setupControls();
+
+		window.onresize = this.resize.bind(this);
+		this.resize();
+
+		requestAnimationFrame(this.render.bind(this));
+	}
+
+	_setupControls() {
+		new OrbitControls(this._camera, this._divContainer);
+	}
+
+	_setupCamera() {
+		const width = this._divContainer.clientWidth;
+		const height = this._divContainer.clientHeight;
+		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+		camera.position.z = 5;
+		this._camera = camera;
+	}
+
+	_setupLight() {
+		const color = 0xffffff;
+		const intensity = 1;
+		const light = new THREE.DirectionalLight(color, intensity);
+		light.position.set(-1, 2, 4);
+		this._scene.add(light);
+	}
+
+	/**
+	 * For PlaneGeometry
+	 *
+	 * * Parameters
+	 *   `width`: (default value: 1)
+	 *   `height`: (default value: 1)
+	 *   `widthSegments`: (default value: 1)
+	 *   `heightSegments`: (default value: 1)
+	 */
+	_setupModel() {
+		const geometry = new THREE.PlaneGeometry(1, 2, 1, 2);
+		const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+		const cube = new THREE.Mesh(geometry, fillMaterial);
+
+		const lineMaterial = new THREE.LineBasicMaterial({ color: 0x39ff14 });
+		const line = new THREE.LineSegments(
+			new THREE.WireframeGeometry(geometry),
+			lineMaterial
+		);
+
+		const group = new THREE.Group();
+		group.add(cube);
+		group.add(line);
+
+		this._scene.add(group);
+		this._cube = group;
+	}
+
+	resize() {
+		const width = this._divContainer.clientWidth;
+		const height = this._divContainer.clientHeight;
+
+		this._camera.aspect = width / height;
+		this._camera.updateProjectionMatrix();
+
+		this._renderer.setSize(width, height);
+	}
+
+	render(time) {
+		this._renderer.render(this._scene, this._camera);
+		this.update(time);
+		requestAnimationFrame(this.render.bind(this));
+	}
+
+	update(time) {
+		time *= 0.001; // second unit
+		// this._cube.rotation.x = time;
+		// this._cube.rotation.y = time;
+	}
+}
+
+window.onload = function () {
+	new App();
+};
